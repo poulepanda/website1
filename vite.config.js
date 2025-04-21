72e1241a8e298c9131eec64eada54d4d3ecea312
poulepanda <poulemega@gmail.com>
2025-04-21 14:48:10 +0300

    v-4

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  },
+  preview: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  }
+})
