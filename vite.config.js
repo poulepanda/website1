b5fd759313ad724f1608882287d5349fec91bdaf
poulepanda <poulemega@gmail.com>
2025-04-21 14:52:09 +0300

    v-5

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
   },
   preview: {
     port: process.env.PORT || 3000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: [
+      'website1.pandanrv.shop',
+      'localhost',
+      '127.0.0.1'
+    ]
   }
 })
